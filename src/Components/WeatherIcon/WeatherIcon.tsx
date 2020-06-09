ce296d08c08d1532085bc121f92ec6e0d367ba06
gazjosef <39395712+gazjosef@users.noreply.github.com>
2020-06-09 17:25:57 +1000

    Fixed Bug

--- src/Components/WeatherIcon/WeatherIcon.tsx
+++ src/Components/WeatherIcon/WeatherIcon.tsx
@@ -2,13 +2,11 @@ import React from "react";
 import "./WeatherIcon.css";
 
 const WeatherIcon = (props: any) => {
-  const icon = `../svg/${props.icon}.svg`
-  console.log(icon)
   if (props.icon !== undefined) {
     return (
       <div id="weather-icon">
         <img
-          // src={require(`${icon}`)}
+          src={require(`./icons/${props.icon}.svg`)}
           alt={props.description}
           className="filter-white"
         />
