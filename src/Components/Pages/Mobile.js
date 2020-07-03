64fbe62541eccdc046380703a500ac23c624604d
gazjosef <39395712+gazjosef@users.noreply.github.com>
2020-07-03 23:30:00 +1000

    Updated Mobile.js

--- src/Components/Pages/Mobile.js
+++ src/Components/Pages/Mobile.js
@@ -3,7 +3,28 @@ import React from 'react';
 export const Mobile = () => {
   return (
     <div>
-      <h1>Hello World</h1>
+      {/* iPhone / iPad */}
+      <div className="mobile">
+        {/* Screen */}
+        <div className={`screen`}>
+          search-field
+          <div className="weather-display">weather-display</div>
+        </div>
+        {/* Home Button */}
+        <div className="home"></div>
+        <div className="inner"></div>
+        {/* Volume */}
+        <ul className="volume">
+          <li></li>
+          <li></li>
+        </ul>
+        <ul className="silent">
+          <li></li>
+        </ul>
+        <ul className="sleep">
+          <li></li>
+        </ul>
+      </div>
     </div>
   );
 };
