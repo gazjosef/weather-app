eeb029b9b59a9d0785b2dd1994978ddd0fb7fbf1
gazjosef <39395712+gazjosef@users.noreply.github.com>
2020-07-03 23:43:29 +1000

    Updated Mobile.js

--- src/Components/Pages/Mobile.js
+++ src/Components/Pages/Mobile.js
@@ -1,30 +1,33 @@
-import React from 'react';
+import React, { Fragment } from 'react';
+import '../../iPhone.css';
 
 export const Mobile = () => {
   return (
-    <div>
-      {/* iPhone / iPad */}
-      <div className="mobile">
-        {/* Screen */}
-        <div className={`screen`}>
-          search-field
-          <div className="weather-display">weather-display</div>
+    <Fragment>
+      <div className="center">
+        {/* iPhone / iPad */}
+        <div className="mobile">
+          {/* Screen */}
+          <div className={`screen`}>
+            search-field
+            <div className="weather-display">weather-display</div>
+          </div>
+          {/* Home Button */}
+          <div className="home"></div>
+          <div className="inner"></div>
+          {/* Volume */}
+          <ul className="volume">
+            <li></li>
+            <li></li>
+          </ul>
+          <ul className="silent">
+            <li></li>
+          </ul>
+          <ul className="sleep">
+            <li></li>
+          </ul>
         </div>
-        {/* Home Button */}
-        <div className="home"></div>
-        <div className="inner"></div>
-        {/* Volume */}
-        <ul className="volume">
-          <li></li>
-          <li></li>
-        </ul>
-        <ul className="silent">
-          <li></li>
-        </ul>
-        <ul className="sleep">
-          <li></li>
-        </ul>
       </div>
-    </div>
+    </Fragment>
   );
 };
