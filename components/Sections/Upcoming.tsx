f628a9392a372f46f906818bfafb0b406a20d813
Gareth Hind <garethjhind@gmail.com>
2024-04-11 19:59:39 +1000

    Fix: TypeScript Bug

--- components/Sections/Upcoming.tsx
+++ components/Sections/Upcoming.tsx
@@ -17,33 +17,33 @@ const Upcoming: React.FC<UpcomingProps> = ({ upcomingData }) => {
     return time;
   };
 
+  if (upcomingData) {
+    console.log("upcomingData", upcomingData);
+  }
+
   return (
     <div className="h-[100px] w-full | flex flex-col justify-between">
       <h1 className="text-sm text-left">Hourly Forecast</h1>
       <div className="daily__hours | flex items-center justify-between">
         {upcomingData &&
-          upcomingData.map(
-            (data: UpcomingData) =>
-              data.list &&
-              data.list.map((entry: WeatherEntry) => (
-                <div
-                  key={entry.dt}
-                  className="hour | bg-sky-500 w-[35px] h-[70px] p-[5px] border-solid rounded-[10px] | flex flex-col items-center justify-between | text-[9px] text-white"
-                >
-                  <div>{timeConverter(entry.dt)}</div>
-                  <IconContext.Provider value={{ size: "2em" }}>
-                    {entry.weather &&
-                      entry.weather.length > 0 &&
-                      iconConverter(entry.weather[0].icon)}
-                  </IconContext.Provider>
-                  <div>
-                    {entry.main &&
-                      JSON.stringify(entry.main.temp - 273.15).slice(0, 2)}
-                    &#8451;
-                  </div>
-                </div>
-              ))
-          )}
+          upcomingData.map((data, index) => (
+            <div
+              key={index}
+              className="hour | bg-sky-500 w-[35px] h-[70px] p-[5px] border-solid rounded-[10px] | flex flex-col items-center justify-between | text-[9px] text-white"
+            >
+              <div>{timeConverter(data.dt)}</div>
+              <IconContext.Provider value={{ size: "2em" }}>
+                {data.weather &&
+                  data.weather.length > 0 &&
+                  iconConverter(data.weather[0].icon)}
+              </IconContext.Provider>
+              <div>
+                {data.main &&
+                  JSON.stringify(data.main.temp - 273.15).slice(0, 2)}
+                &#8451;
+              </div>
+            </div>
+          ))}
       </div>
     </div>
   );
